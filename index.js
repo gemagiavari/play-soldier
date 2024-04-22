d6e497700c4f67c06f78e85d03acb99c131d5a0c
Gema Giavari <52576980+gemagiavari@users.noreply.github.com>
2024-04-22 10:46:51 +0700

    add animation to the play button

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
 document.getElementById("playButton").addEventListener("click", playSoldier);
 
 function playSoldier() {
+  //Animation for the "play" button
+  this.classList.add("button-clicked");
 
-    let randomNumber1 = Math.floor(Math.random() * 3) + 1; //random number between 1-3
+  // Remove the class after the animation is done
+  setTimeout(() => {
+    this.classList.remove("button-clicked");
+  }, 300); // The same duration as your animation
 
-let randomSoldierImage1 = "soldier" + randomNumber1 + ".png"; //generate string soldier1.png - soldier3.png
+  let randomNumber1 = Math.floor(Math.random() * 3) + 1; //random number between 1-3
 
-let randomImageSource1 = "images/" + randomSoldierImage1;
+  let randomSoldierImage1 = "soldier" + randomNumber1 + ".png"; //generate string soldier1.png - soldier3.png
 
-let image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src", randomImageSource1);
+  let randomImageSource1 = "images/" + randomSoldierImage1;
 
+  let image1 = document.querySelectorAll("img")[0];
+  image1.setAttribute("src", randomImageSource1);
 
-let randomNumber2 = Math.floor(Math.random() * 3) + 1; //random number between 1-3
+  let randomNumber2 = Math.floor(Math.random() * 3) + 1; //random number between 1-3
 
-let randomSoldierImage2 = "soldier" + randomNumber2 + ".png"; //generate string soldier1.png - soldier3.png
+  let randomSoldierImage2 = "soldier" + randomNumber2 + ".png"; //generate string soldier1.png - soldier3.png
 
-let randomImageSource2 = "images/" + randomSoldierImage2;
+  let randomImageSource2 = "images/" + randomSoldierImage2;
 
-let image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src", randomImageSource2);
+  let image2 = document.querySelectorAll("img")[1];
+  image2.setAttribute("src", randomImageSource2);
 
-
-if (randomNumber1 > randomNumber2) {
+  if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
-} else if (randomNumber2 > randomNumber1) {
+  } else if (randomNumber2 > randomNumber1) {
     document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-} else {
+  } else {
     document.querySelector("h1").innerHTML = "🚩 Draw! 🚩";
-}
+  }
 
-
-// Play the sound
-let playSound = document.getElementById("playSound");
-playSound.play();
-
+  // Play the sound
+  let playSound = document.getElementById("playSound");
+  playSound.play();
 }
